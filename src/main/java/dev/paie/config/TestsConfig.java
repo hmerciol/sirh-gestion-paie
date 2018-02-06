4e4a6691bb0b1027f8ac6bf50389aa67ce4e4612
hmerciol <li.henri@merciol.fr>
2018-02-06 15:26:06 +0100

    modifications mineures

--- src/main/java/dev/paie/config/TestsConfig.java
+++ src/main/java/dev/paie/config/TestsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @Import({ServicesConfig.class})
-@ImportResource("jdd-config.xml")
+@ImportResource("classpath:jdd-config.xml")
 public class TestsConfig {
 
 }
